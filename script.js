9b861d84aee52285899283c302a45edb9d7a2b11
Tuan Kiet Le <86156449+kademy-software@users.noreply.github.com>
2023-07-05 10:55:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; 
+const API_KEY = "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"; 
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
